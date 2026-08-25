821c7d58ccd062ab4e95ce0b1d94fb2f16ae6257
Arwa alhomrani <lla644183@gmail.com>
2026-08-26 02:16:12 +0300

    Refactor parser.c by removing unused functions
    Removed the read_line and free_args functions. Updated split_line to use position instead of i for token indexing.

--- parser.c
+++ parser.c
@@ -1,72 +1,33 @@
 #include "shell.h"
 
 /**
- * read_line - Reads a line of input from standard input
+ * split_line - Splits a line into tokens using DELIM
+ * @line: The input line to parse
  *
- * Return: Pointer to the read line, or NULL on EOF/error
- */
-char *read_line(void)
-{
-	char *line = NULL;
-	size_t bufsize = 0;
-	ssize_t nread;
-
-	nread = getline(&line, &bufsize, stdin);
-	if (nread == -1)
-	{
-		free(line);
-		return (NULL);
-	}
-
-	return (line);
-}
-
-/**
- * split_line - Splits a line into tokens (arguments)
- * @line: The input string to split
- *
- * Return: Null-terminated array of tokens, or NULL on failure
+ * Return: Array of string pointers (tokens), or NULL on failure
  */
 char **split_line(char *line)
 {
-	int bufsize = 64, i = 0;
+	int bufsize = 64, position = 0;
 	char **tokens = malloc(bufsize * sizeof(char *));
 	char *token;
 
 	if (!tokens)
-	{
-		perror("Allocation error");
 		return (NULL);
-	}
 
 	token = strtok(line, DELIM);
 	while (token != NULL)
 	{
-		tokens[i] = token;
-		i++;
-
-		if (i >= bufsize)
+		tokens[position++] = token;
+		if (position >= bufsize)
 		{
 			bufsize += 64;
 			tokens = realloc(tokens, bufsize * sizeof(char *));
 			if (!tokens)
-			{
-				perror("Allocation error");
 				return (NULL);
-			}
 		}
 		token = strtok(NULL, DELIM);
 	}
-	tokens[i] = NULL;
+	tokens[position] = NULL;
 	return (tokens);
-}
-
-/**
- * free_args - Frees the memory allocated for arguments array
- * @args: Array of strings
- */
-void free_args(char **args)
-{
-	if (args)
-		free(args);
 }
