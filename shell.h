516225e85514acf6f342e3bb250a8bd1fc99a79b
Arwa alhomrani <lla644183@gmail.com>
2026-08-25 05:36:05 +0300

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define DELIM " \t\r\n\a"
+
+extern char **environ;
+
+/* Function prototypes */
+char *read_line(void);
+char **split_line(char *line);
+int execute_cmd(char **args, char *prog_name);
+void free_args(char **args);
+
+#endif /* SHELL_H */
