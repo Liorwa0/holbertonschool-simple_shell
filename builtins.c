e8e29924e23b1b0990735b7d25711cc17181b0c5
Arwa alhomrani <lla644183@gmail.com>
2026-08-31 12:16:02 +0300

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,19 +1,25 @@
 #include "shell.h"
 
-/*
+/**
  * handle_builtin - Runs a built-in if args[0] is one
  * @args: Null-terminated array of arguments
  * @line: Buffer from read_line, freed before exiting
+ * @last: Exit status of the last executed command
  *
  * Return: 1 if a built-in was handled, 0 otherwise
  */
 int handle_builtin(char **args, char *line, int last)
 {
+	int status = last;
+
 	if (strcmp(args[0], "exit") == 0)
 	{
+		if (args[1] != NULL)
+			status = atoi(args[1]);
+
 		free_args(args);
 		free(line);
-		exit(last);
+		exit(status);
 	}
 
 	if (strcmp(args[0], "env") == 0)
@@ -32,7 +38,6 @@ void print_env(void)
 {
 	int i;
 
-	for (i = 0; environ[i] != NULL; i++)
+	for (i = 0; environ && environ[i] != NULL; i++)
 		printf("%s\n", environ[i]);
 }
-
